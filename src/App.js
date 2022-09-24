6a7068add32df38b4b2c7798dfc300d0be1c3d56
MoeElItani <mhmd98itani@gmail.com>
2022-09-24 11:58:37 +0300

    Basic App Structure & Routings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import './App.scss';
+// Imports
+import React from 'react'
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+} from 'react-router-dom'
+// Components
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetails from './components/MovieDetails/MovieDetails'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Footer from './components/Footer/Footer'
+// Styles
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
+   return (
+      <div className='App'>
+         <Router>
+            <Header />
+            <Routes>
+               {/* Home Page */}
+               <Route path='/' exact element={<Home />} />
+               {/* Movie Details Page */}
+               <Route
+                  path='/movie/:imdbID'
+                  element={<MovieDetails />}
+               />
+               {/* 404 Page Not Found  / Wrong URL */}
+               <Route path='*' element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+         </Router>
+      </div>
+   )
 }
 
-export default App;
+export default App
