7829e802d9e8c738872963facf9fd592716b86d7
MoeElItani <mhmd98itani@gmail.com>
2022-09-27 18:14:43 +0300

    Async Thunk & minor fixes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,18 @@
 // Imports
-import React from 'react'
-import { useEffect } from 'react'
+import React, {useEffect } from 'react'
 // Components
 import MovieListing from '../MovieListing/MovieListing'
-import movieApi from '../../api/movieApi'
-import { key } from '../../api/movieApiKey'
 // Redux Toolkit
 import { useDispatch } from 'react-redux'
-import { addMovies } from '../../features/movies/movieSlice'
+import { fetchAsyncMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
-   const sample = 'web'
    const dispatch = useDispatch()
+
    useEffect(() => {
-      const fetchMovies = async () => {
-         const response = await movieApi
-            .get(`?apikey=${key}&s=${sample}&type=series`)
-            .catch((err) => {
-               console.log('Error: ', err)
-            })
-         dispatch(addMovies(response.data))
-      }
-      fetchMovies()
-   }, [dispatch])
+      dispatch(fetchAsyncMovies())
+   }, [ dispatch ])
+   
    return (
       <>
          <div className='banner-img'></div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
+import {
+   createSlice,
+   createAsyncThunk,
+} from '@reduxjs/toolkit'
+import movieApi from '../../api/movieApi'
+import { key } from '../../api/movieApiKey'
+
+export const fetchAsyncMovies = createAsyncThunk(
+   'movies/fetchAsyncMovies',
+   async () => {
+      const sample = 'london'
+
+      const response = await movieApi.get(
+         `?apikey=${key}&s=${sample}&type=series`
+      )
+      return response.data
+   }
+)
 
 const initialState = {
    movies: {},
@@ -10,6 +27,21 @@ const movieSlice = createSlice({
    reducers: {
       addMovies: (state, { payload }) => {
          state.movies = payload
+      },
+   },
+   extraReducers: {
+      [fetchAsyncMovies.pending]: () => {
+         console.log('pending')
+      },
+      [fetchAsyncMovies.fulfilled]: (
+         state,
+         { payload }
+      ) => {
+         console.log('success')
+         return { ...state, movies: payload }
+      },
+      [fetchAsyncMovies.rejected]: () => {
+         console.log('failed')
       },
    },
 })
